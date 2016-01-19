6048bf3dd9869b071d26690db199a4100b882403
Joey Laurent <joey@pixar.com>
2016-01-19 13:17:11 -0800

    fixed playerStandings() to accept any number of pairs, add custom db class for simple db queries.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -5,6 +5,40 @@
 
 import psycopg2
 
+class DB:
+
+    def __init__(self, db_con_str="dbname=tournament"):
+        """
+        Creates a database connection with the connection string provided
+        :param str db_con_str: Contains the database connection string, with a default value when no argument is passed to the parameter
+        """
+        self.conn = psycopg2.connect(db_con_str)
+
+    def cursor(self):
+        """
+        Returns the current cursor of the database
+        """
+        return self.conn.cursor();
+
+    def execute(self, sql_query_string, and_close=False):
+        """
+        Executes SQL queries
+        :param str sql_query_string: Contain the query string to be executed
+        :param bool and_close: If true, closes the database connection after executing and commiting the SQL Query
+        """
+        cursor = self.cursor()
+        cursor.execute(sql_query_string)
+        if and_close:
+            self.conn.commit()
+            self.close()
+        return {"conn": self.conn, "cursor": cursor if not and_close else None}
+
+    def close(self):
+        """
+        Closes the current database connection
+        """
+        return self.conn.close()
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,20 +47,12 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database. delete from matches;"""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("DELETE from matches;")
-    conn.commit()
-    conn.close()
+    DB().execute("DELETE from matches", True)
 
 
 def deletePlayers():
     """Remove all the player records from the database.delete from players;"""
-    conn = connect()
-    c = conn.cursor()
-    c.execute("DELETE from players;")
-    conn.commit()
-    conn.close()
+    DB().execute("DELETE from players", True)
 
 
 def countPlayers():
@@ -56,6 +82,7 @@ def registerPlayer(name):
     conn.commit()
     conn.close()
 
+
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
@@ -71,7 +98,7 @@ def playerStandings():
     """
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT id, name, (SELECT count(*) as wins FROM matches WHERE matches.winner = players.id), ((SELECT count(*) as wins FROM matches WHERE matches.winner = players.id)+ (SELECT count(*) as losses FROM matches WHERE matches.loser = players.id)) AS total FROM players order by wins desc;")
+    c.execute("SELECT * from standings;")
     standings = c.fetchall()
     conn.close()
     return standings
@@ -108,12 +135,17 @@ def swissPairings():
         name2: the second player's name
 
     """
+    pairings = []
     pstand = playerStandings()
-    pair1 = (pstand[0][0], pstand[0][1], pstand[1][0], pstand[1][1])
-    pair2 = (pstand[2][0], pstand[2][1], pstand[3][0], pstand[3][1])
-    pairings = [pair1, pair2]
-    print pairings
+    a = 0
+    b = 1
+    for x in xrange(0,len(pstand)/2):
+        result = (pstand[a][0], pstand[a][1], pstand[b][0], pstand[b][1])
+        pairings.append(result)
+        a = a+2
+        b = b+2
     return pairings
+
 
 
 
